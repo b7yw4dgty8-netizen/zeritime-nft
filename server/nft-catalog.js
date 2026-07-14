564f29552ca954d60b9da76d80652ae1c39b86c2
Swarrow 1481 <exit@MacBook-Air-Swarrow.local>
2026-07-14 16:47:08 +0300

    new blue cube

--- server/nft-catalog.js
+++ server/nft-catalog.js
@@ -30,4 +30,12 @@ module.exports = [
     imageUrl: 'https://i.ibb.co/qMbgmBng/146121854812165290.png',
     description: 'I drawed it for memory about my old owl pet',
   },
+  {
+    id: 'blue-cube',
+    name: 'Blue Cube',
+    price: '5000',
+    emoji: '🧊',
+    imageUrl : 'https://i.ibb.co/q3fMnM1h/3d-rendering-geometric-cube-23-2150979614-jpg.avif',
+    descriptiom: 'Blue Cube is a cube that is blue',
+  },
 ];
